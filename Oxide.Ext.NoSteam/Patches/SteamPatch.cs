4b2058761ee300625abe6b1bda28256df8bcd35c
I4IgO32 <58877754+I4IgO32@users.noreply.github.com>
2024-01-22 22:25:44 +1200

    Add files via upload

--- Oxide.Ext.NoSteam/Patches/SteamPatch.cs
+++ Oxide.Ext.NoSteam/Patches/SteamPatch.cs
@@ -164,8 +164,8 @@ namespace Oxide.Ext.NoSteam.Patches
                 var connections = ConnectionAuth.m_AuthConnection;
                 var connection = connections.First(x => x.userid == userId);
 
-                //connection.authStatusEAC= "ok";
-                //connection.authStatusSteam = "ok";
+                connection.authStatusCentralizedBans = "ok";
+                connection.authStatusSteam = "ok";
 
                 __result = true;
 
